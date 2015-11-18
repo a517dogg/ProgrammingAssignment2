cd1599aa2cfd0da949bf0e669c9b66f3cfd1a4ad
a517dogg <adrian.r.martin@gmail.com>
2015-11-18 15:55:41 -0500

    first commit, not sure the assignment works yet

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    #This function creates a special "matrix" object that can cache its inverse.
+    check <<- TRUE
+    invm <<- solve(x)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    #This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+    #If the inverse has already been calculated (and the matrix has not changed), 
+    #then the cachesolve should retrieve the inverse from the cache.
+    
+    #compute the inverse of the special matrix returned by makeCacheMatrix
+    if(exists("check")) {
+        message("inverse already calculated")
+        print(invm)
+        print("printing invm")
+    }
+    else {
+        print("printing x")
+        solve(x)
+    }
 }
